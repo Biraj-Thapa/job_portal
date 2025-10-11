8a0c5a2fb476c59377d558d59b4a576b8e7e885a
Biraj Thapa <birajthapa983@gmail.com>
2025-10-11 20:14:07 +0545

    register login backend completed

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,63 @@
+import  prisma  from "../prismaClient.js";
+import bcrypt from "bcrypt";
+import { generateToken } from "../utils/generateToken.js";
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    if (!name || !email || !password || !role) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        role,
+      },
+    });
+
+    res.status(201).json({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      token: generateToken(user.id),
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+
+    res.json({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      token: generateToken(user.id),
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 import express from "express"
 import dotenv from "dotenv";
-import { PrismaClient } from "./generated/prisma/index.js";
+import cors from "cors"
+import authRoutes from "./routes/auth.route.js";
 
 
 
 const app=express()
-const prisma = new PrismaClient();
 
 app.use(express.json());
 dotenv.config();
@@ -13,18 +13,8 @@ dotenv.config();
 
 const PORT = process.env.PORT || 6000
 
-app.post("/api/register", async (req, res) => {
-  const { name, email, password, role } = req.body;
-  try {
-    const user = await prisma.user.create({
-      data: { name, email, password, role },
-    });
-    res.json({ success: true, user });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ success: false, error: "User creation failed" });
-  }
-});
+app.use("/api/auth", authRoutes);
+
 
 app.listen(PORT, ()=>{
 console.log(`listening on port ${PORT}`)
