ba9b1ebb7401aa519cf572a8a08ff38e59eddb17
Biraj Thapa <birajthapa983@gmail.com>
2025-10-11 13:40:50 +0545

    db connected

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,30 @@
 import express from "express"
 import dotenv from "dotenv";
+import { PrismaClient } from "./generated/prisma/index.js";
 
 
 
 const app=express()
+const prisma = new PrismaClient();
+
+app.use(express.json());
 dotenv.config();
 
 
 const PORT = process.env.PORT || 6000
 
-app.get("/",(req,res)=>{
-res.send("hello world")
-})
+app.post("/api/register", async (req, res) => {
+  const { name, email, password, role } = req.body;
+  try {
+    const user = await prisma.user.create({
+      data: { name, email, password, role },
+    });
+    res.json({ success: true, user });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, error: "User creation failed" });
+  }
+});
 
 app.listen(PORT, ()=>{
 console.log(`listening on port ${PORT}`)
